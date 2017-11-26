1e8935b095767439e67a6b84a4c44c3f047ec61c
ignazioc <ignazioc@gmail.com>
2017-11-26 22:24:41 +0100

    Skip non-finalized turnover

--- lib/moneymanager/parser.rb
+++ lib/moneymanager/parser.rb
@@ -29,15 +29,22 @@ module Moneymanager
     def parse
       options = { headers: true, return_headers: false, header_converters: :symbol, converters: :all, col_sep: ';' }
       entries = []
+      invalid = 0
       CSV.foreach(@local_file, options) do |row|
-        entry = Entry.new
-        entry.date = Date.strptime(row[:buchungstag], '%d.%m.%y')
-        entry.reason = row[:verwendungszweck].squeeze(' ')
-        entry.amount = row[:betrag].to_s.tr(',', '.').to_f
-        entry.company = row[:beguenstigterzahlungspflichtiger].squeeze(' ')
-        entry.raw = row.to_csv.squeeze(' ').strip
-        entries << entry
+        
+        if row[:info] == "Umsatz gebucht"
+          entry = Entry.new
+          entry.date = Date.strptime(row[:buchungstag], '%d.%m.%y')
+          entry.reason = row[:verwendungszweck].squeeze(' ')
+          entry.amount = row[:betrag].to_s.tr(',', '.').to_f
+          entry.company = row[:beguenstigterzahlungspflichtiger].squeeze(' ')
+          entry.raw = row.to_csv.squeeze(' ').strip
+          entries << entry
+        else
+          invalid += 1 
+        end
       end
+      puts "Invalid: #{invalid}"
       entries
     end
   end
