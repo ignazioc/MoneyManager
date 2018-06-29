8b89b3f2bf0dcddb7ac607937e44abe85626eb6d
IgnazioC <ignazioc@gmail.com>
2018-06-29 16:07:23 +0200

    Suggest tags

--- lib/moneymanager/tagger.rb
+++ lib/moneymanager/tagger.rb
@@ -6,6 +6,7 @@ class Tagger
   def retag(entries)
     
     archiver = Moneymanager::Archiver.new
+    tag_guesser = Moneymanager::TaggerGuesser.new(archiver.all_entries)
 
     prompt = TTY::Prompt.new
     entries.each do |entry|
@@ -14,13 +15,22 @@ class Tagger
       Layout.print_single(entry)
       
       options = default_options.concat(archiver.tags.sort!).map { |x| x.to_s }
+
+      suggested_tag = tag_guesser.guess(entry)
+      if suggested_tag.nil? 
+        suggested_tag = "Varie"
+      end
+      options = options.unshift("Suggested: #{suggested_tag}")
+
       action = prompt.select('Do you want add a tags?', options, per_page: 30)
 
       if action == options[0]
-        # NOP
+        entry.tag = suggested_tag
       elsif action == options[1]
-        exit
+        # NOP
       elsif action == options[2]
+        exit
+      elsif action == options[3]
         tag = prompt.ask('Create a new tag') do |q|
           q.required true
           q.validate(/^\S*$/)
